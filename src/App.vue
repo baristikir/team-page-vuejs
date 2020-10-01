e1330a9448848e0682023ffd8e81514b6a791998
Baris Tikir <brstkr@icloud.com>
2020-10-01 15:56:09 +0200

    Project Setup :pushpin:

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Header/>
+  <Cards/>
+</template>
+
+<script>
+import Cards from './components/Cards.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Cards
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="header">
+        <p>Hello From Header</p>
+    </div>
+</template>
+
+<style lang="scss">
+    $primary-color: black;
+
+    #header{
+        background: $primary-color;
+    }
+</style>
